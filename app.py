16dc9853e5037742c629cd4a587d8be9bd7d5229
I.K <32903511+rt-fan@users.noreply.github.com>
2024-05-11 16:17:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import json
-
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/get_time')
+def get_time():
     with open('data.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
         time = data['time']
         people = data['people']
-        return render_template('index.html', time=time, people=people)
-
+        return jsonify({'time': time})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
