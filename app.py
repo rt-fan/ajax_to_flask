42907c794cae70ce18a623d37e9d78dbb90cb9fb
I.K <32903511+rt-fan@users.noreply.github.com>
2024-05-11 15:29:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 import json
+
+
 app = Flask(__name__)
+
+
 @app.route('/')
 def index():
     with open('data.json', 'r', encoding='utf-8') as file:
@@ -8,5 +12,7 @@ def index():
         time = data['time']
         people = data['people']
         return render_template('index.html', time=time, people=people)
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
